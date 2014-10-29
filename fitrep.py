cdb08895cb2df0c4111ca2fdd927283812ddb781
MichaelAHood <Michael.A.Hood314@gmail.com>
2014-10-29 14:05:28 -0400

    Removed Pandas dependencies and used CSV module

--- fitrep.py
+++ fitrep.py
@@ -2,7 +2,8 @@
 """
 Created on Thu Aug 07 13:08:18 2014
 
-@author: mhood2
+@author: Michael Hood
+email: michael.allen.hood314@gmail.com
 """
 ##############################################################################
 
@@ -21,23 +22,39 @@ Created on Thu Aug 07 13:08:18 2014
 ###############################################################################
 
 
-# Part 1: The introduction line
-
-#top_three_adjectives = ['top-notch', ' ]
-
-# if #1
-# "MIDN X has distinguished himself as the top-performer/head of the class
-# during the last semester.
 
 
+def create_dict(file_name):
+    #First, read the csv file and write to rows to a list called stud_data
+    import csv
+    stud_data = []
 
-# Get the MIDN name and sex appropraite pronoun, e.g. he, his, she, her
-def get_name(df, index):
-    return df.at[index, 'Last_Name']
+    with open(file_name, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            stud_data.append(row)
+
+#Next, create a data structure, stud_dict w/ the CSV column headers as the keys, and write the column values to dict 
+    stud_dict = {}
+
+    for field in stud_data[0]: stud_dict[field] = []
+
+    for field in range(len(stud_data[0])):
+        key = stud_data[0][field]  
+        for record in range(1, len(stud_data)):
+            stud_dict[key].append(stud_data[record][field])
+
+    return stud_dict
+
+
+
+# Get the MIDN name and sex appropriate pronoun, e.g. he, his, she, her
+def get_name(stud_dict, index):
+    return stud_dict['Last_Name'][index]
     
-def get_pronouns(df, index):
+def get_pronouns(stud_dict, index):
     
-    sex = df.at[index, 'Sex']
+    sex = stud_dict['Sex'][index]
     
     if sex == 'M':
         pronouns = ['he', 'his', 'He', 'His']
@@ -48,11 +65,12 @@ def get_pronouns(df, index):
     
 
 # Get the aptitude ranking of a mid and at the given index
-def get_ranking(df, index): 
-    return df.at[index, 'Ranking']
+def get_ranking(stud_dict, index):
+    return stud_dict['Ranking'][index]
 
-def get_class_size(df):
-    return df.Ranking.count()
+def get_class_size(stud_dict):
+    return len(stud_dict['Last_Name'])
+
     
     
 # Determine the sentiment of the opening line by which third of the rankings the mid # is in
@@ -73,23 +91,26 @@ def determine_third(class_size, ranking):
     return third
     
 
-def get_class(df, index):
-    return df.at[index, 'Class']
+def get_class(stud_dict, index):
+    return stud_dict['Class'][index]
 
 # If MIDN is ranked #1, he gets a special intro
 #####################################################################
 #####################################################################
-def make_intro_line(df, index):
+def make_intro_line(stud_dict, index):
     
-    last_name = get_name(df, index) 
-    ranking = get_ranking(df, index) 
-    class_size = get_class_size(df) 
+    last_name = get_name(stud_dict, index) 
+    ranking = int(get_ranking(stud_dict, index)) 
+    class_size = get_class_size(stud_dict) 
     third = determine_third(class_size, ranking)    
-    grade_level = get_class(df, index)
+    grade_level = get_class(stud_dict, index)
     
     
     if ranking == 1:
         statement = """MIDN %s has finished the semester as the top midshipman in the %s class!""" % (last_name, grade_level)
+    
+    elif int(ranking) == class_size:
+        statement = "MIDN %s has finished the semester at the bottom of the %s class." % (last_name, grade_level)
     
     elif third == 1:
         statement = "MIDN %s has finished the semester in the top third of the %s class." % (last_name, grade_level)
@@ -100,30 +121,26 @@ def make_intro_line(df, index):
     elif third == 3:
         statement = "MIDN %s has finished the semester in the bottom third of the %s class." % (last_name, grade_level)
         
-    elif ranking == len(df) - 1:
-        statement = "MIDN %s has finished the semester at the bottom of the %s class." % (last_name, grade_level)
-        return statement
-     
     return statement  
 ### Remember to address conduct issues and PRB's at a later point.
 #############################################################################   
 #############################################################################
-def get_gpa(df, index):
-    return df.at[index, 'Term_GPA']
+def get_gpa(stud_dict, index):
+    return stud_dict['Term_GPA'][index]
 
 
-def get_major(df, index):
-    return df.at[index, 'Major']
+def get_major(stud_dict, index):
+    return stud_dict['Major'][index]
 
 
-def get_prt(df, index):
-    return df.at[index, 'PRT']
+def get_prt(stud_dict, index):
+    return stud_dict['PRT'][index]
     
-def get_conduct(df, index):
-    return df.at[index, 'PRB']
+def get_conduct(stud_dict, index):
+    return stud_dict['PRB'][index]
     
-def get_tardy(df, index):
-    return df.at[index, 'Tardy_To_NROTC_Events']
+def get_tardy(stud_dict, index):
+    return stud_dict['Tardy_To_NROTC_Events'][index]
 
 
 ##################################################################
@@ -162,7 +179,7 @@ def make_second_line(df, index):
         article = "an"
     else:
         article = 'a'
-    gpa = get_gpa(df, index)
+    gpa = float(get_gpa(df, index))
     
 
     
@@ -249,15 +266,16 @@ def professional_comment(df, index):
 #####################################################################
         
 ######################################################################
-def complete_comment(df, index):
+def complete_comment(stud_dict, index):
     
-    first_line = make_intro_line(df, index)
-    second_line = make_second_line(df, index)
-    last_line = make_last_line(df, index)
+    first_line = make_intro_line(stud_dict, index)
+    second_line = make_second_line(stud_dict, index)
+    last_line = make_last_line(stud_dict, index)
+    class_size = get_class_size(stud_dict)
     
-    pronouns = get_pronouns(df, index)    
-    ranking = get_ranking(df, index)
-    ranking_comment = "As a result of %s performance, %s is ranked %d of %d in %s class." % (pronouns[1], pronouns[0], ranking, len(df), pronouns[1])    
+    pronouns = get_pronouns(stud_dict, index)    
+    ranking = get_ranking(stud_dict, index)
+    ranking_comment = "As a result of %s performance, %s is ranked %d of %d in %s class." % (pronouns[1], pronouns[0], int(ranking), class_size, pronouns[1])    
     
     return first_line + "  " + second_line + "  " + last_line + ranking_comment    
     
--- writer.py
+++ writer.py
@@ -4,30 +4,28 @@ Created on Fri Oct 10 08:33:16 2014
 
 @author: mhood2
 """
-import pandas
+
 import fitrep
 import csv
 
 file_name = raw_input("What is the name of the file with the student data in it? \nNote: This file must end in '.csv'.  Ex: student_data.csv : " )
 
-df = pandas.read_csv(file_name)
+stud_dict = fitrep.create_dict(file_name)
 
-
-comments = [["Comments"]]
+comments = []
 destination_file_name = raw_input("What is the name of the file that you want the comments written to? \nNote: Your filename must end in '.csv'.  Ex: my_comments.csv :  " )
 
-number_students = len(df)
+number_students = len(stud_dict['Last_Name'])
 
-try:
-    for student in range(number_students):
-        comments.append([fitrep.complete_comment(df, student)])
+#try:
+for student in range(number_students):
+    comments.append([fitrep.complete_comment(stud_dict, student)])
     
-    with open(destination_file_name, 'wb') as destination_file:
-        csv_writer = csv.writer(destination_file)
-        for i in range(0, number_students):
-            csv_writer.writerow(comments[i])
+dest_file = open(destination_file_name, 'wb')
+csv_writer = csv.writer(dest_file)
+for i in range(0, number_students):
+    csv_writer.writerow(comments[i])
 
-finally:
-    destination_file.close()
+dest_file.close()
 
 print "Comments successfully written to %s." % (destination_file_name) 
